eefc3e670bced66c557377d1102974d73d4413c3
Ervin Picardal <ervinjohnpicardal@gmail.com>
2024-08-07 09:23:04 +0800

    first commit

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,30 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function ()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { 
+                    "lua_ls",
+                    "clangd",
+                    "bashls"
+                }
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require('lspconfig')
+            lspconfig.lua_ls.setup({})
+            lspconfig.clangd.setup({})
+            lspconfig.bashls.setup({})
+        end
+    }
+}
