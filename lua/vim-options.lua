4a720cba6929407dd83a9d5a3d1a304fe8ed3de9
Ervin Picardal <ervinjohnpicardal@gmail.com>
2024-08-23 17:25:36 +0800

    ughhh

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,6 +6,7 @@ vim.cmd("set nu")
 vim.cmd("syntax on")
 vim.cmd("set guicursor=")
 vim.cmd("set mouse=")
+vim.cmd("set relativenumber")
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
