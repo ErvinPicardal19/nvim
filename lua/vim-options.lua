48cc5543d4fe85ef80e934c39bdd63a8b815af47
Ervin Picardal <ervinjohnpicardal@gmail.com>
2024-10-24 15:33:55 +0800

    Added tabs navigation

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,3 +1,5 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
@@ -7,8 +9,10 @@ vim.cmd("syntax on")
 vim.cmd("set guicursor=")
 vim.cmd("set mouse=")
 vim.cmd("set relativenumber")
+vim.cmd("")
+vim.cmd("nnoremap <leader>t :tabnew<CR>")
+vim.cmd("nnoremap <leader>] :tabn<CR>")
+vim.cmd("nnoremap <leader>[ :tabp<cr>")
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
